7cc08a7513d6964878943641bc0b2ec0e93c81c5
jeffreygreen <mrjeffreygreen@gmail.com>
2022-05-16 13:13:29 -0500

    Successfully imported static data and passed it with props in the ComicShow page

--- src/pages/ComicShow.jsx
+++ src/pages/ComicShow.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function ComicShow() {
+function ComicShow(props) {
+
+    console.log(props)
+
+    const ComicItemData = props.Comics.data.results[0]
+    console.log(ComicItemData)
+
   return (
-    <div>ComicShow</div>
+    <div>
+        <h1>Comic Show Page</h1>
+        <h2>Title: {ComicItemData.title}</h2>
+        <img src={`${ComicItemData.thumbnail.path}.${ComicItemData.thumbnail.extension}`} alt={ComicItemData.title} />
+        <p>{`Price: $  ${ComicItemData.prices[0].price}`}</p>
+        <h6>Writers:</h6>
+        <p>{`${ComicItemData.creators.items[0].name}, ${ComicItemData.creators.items[1].name}`}</p>
+    </div> 
   )
 }
 
