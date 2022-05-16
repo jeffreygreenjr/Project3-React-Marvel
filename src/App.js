a122e79621d6b40d2ce555a3c05469ef8bd9ff06
jeffreygreen <mrjeffreygreen@gmail.com>
2022-05-16 08:43:37 -0500

    Created and imported a header/footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Explore the Marvel Universe</h1>
+      <Footer />
     </div>
   );
 }
